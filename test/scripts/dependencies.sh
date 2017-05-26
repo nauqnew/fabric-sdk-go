9a9118173cd6df6886eeefa630b139fea0ba4b1f
nauqnew <dozenthree@gmail.com>
2017-05-26 11:30:46 +0800

    go get golang.org/x/tools problem fixed.

--- test/scripts/dependencies.sh
+++ test/scripts/dependencies.sh
@@ -6,4 +6,5 @@ go get -u github.com/axw/gocov/...
 go get -u github.com/AlekSi/gocov-xml
 go get -u github.com/client9/misspell/cmd/misspell
 go get -u github.com/golang/lint/golint
-go get -u golang.org/x/tools/cmd/goimports
+#go get -u github.com/golang/tools  # cp tools to golang.org/x
+go build golang.org/x/tools/cmd/goimports #wen
